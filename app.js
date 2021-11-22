c6902cefa130f4dbd2481064f263cfb46ed4ea03
EdwardWebDev <edwardethridge1@gmail.com>
2021-11-22 03:14:19 -0500

    broken git repo again

---
+++ app.js
@@ -0,0 +1,17 @@
+const grid = document.querySelector('#container');
+for (let i = 0; i < 256; i++) {
+    const sketchDiv = document.createElement('div');
+    grid.appendChild(sketchDiv);
+}
+
+const boxes = document.querySelectorAll('#container div');
+boxes.forEach(box => {
+    box.classList.add('inactive');
+});
+
+boxes.forEach(box => {
+    box.addEventListener('mouseover', function(){
+        box.classList.remove('inactive');
+        box.classList.add('activated');
+    })
+});
