73d2d5523c1f264bcbf5d6564f8869088da78c3a
EdwardWebDev <edwardethridge1@gmail.com>
2021-11-24 05:40:15 -0500

    new logic for creating grid

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const grid = document.querySelector('#container');
-for (let i = 0; i < 256; i++) {
-    const sketchDiv = document.createElement('div');
-    grid.appendChild(sketchDiv);
-}
+// copied and repurposed from Stack Overflow user Nidhin Joseph
+const container = document.querySelector("#container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(16, 16);
 
 const boxes = document.querySelectorAll('#container div');
 boxes.forEach(box => {
